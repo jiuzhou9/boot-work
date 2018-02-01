9adf0d034388ce2b03d77899ca65d86215857931
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-02-01 16:42:36 +0800

    增加api，增加rest api测试接口hello

--- rest-api/src/main/java/com/jiuzhou/bootwork/App.java
+++ rest-api/src/main/java/com/jiuzhou/bootwork/App.java
@@ -1,13 +1,16 @@
 package com.jiuzhou.bootwork;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@SpringBootApplication
+public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
