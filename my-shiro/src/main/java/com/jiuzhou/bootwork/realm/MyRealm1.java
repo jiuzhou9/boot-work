1ee927364bad7c24a11c5fd8838981e0e8bb0160
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-03-02 11:33:02 +0800

    增加shiro

---
+++ my-shiro/src/main/java/com/jiuzhou/bootwork/realm/MyRealm1.java
@@ -0,0 +1,66 @@
+package com.jiuzhou.bootwork.realm;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @author wangjiuzhou (jiuzhou@shanshu.ai)
+ * @date 2018/02/27
+ */
+@Slf4j
+public class MyRealm1 implements Realm {
+
+    /**
+     * 返回一个唯一的realm名字
+     * @return
+     */
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    /**
+     * 判断此 Realm 是否支持此 Token
+     * @param authenticationToken
+     * @return
+     */
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        //仅支持 UsernamePasswordToken 类型的 Token
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    /**
+     * 根据 Token 获取认证信息
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken)
+                    throws AuthenticationException {
+
+        //得到用户名
+        String username = (String)authenticationToken.getPrincipal();
+        //得到密码
+        String password = new String((char[])authenticationToken.getCredentials());
+        if(!"zhang".equals(username)) {
+            //如果用户名错误
+            throw new UnknownAccountException();
+        }
+        if(!"123".equals(password)) {
+            //如果密码错误
+            throw new IncorrectCredentialsException();
+        }
+        //如果身份认证验证成功，返回一个 AuthenticationInfo 实现;
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
--- my-test/src/test/java/com/jiuzhou/bootwork/testinteger/TestInteger.java
+++ my-test/src/test/java/com/jiuzhou/bootwork/testinteger/TestInteger.java
@@ -4,8 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.jiuzhou.bootwork.testnull.Person;
 import junit.framework.TestCase;
 
-import java.text.DecimalFormat;
-
 /**
  * @author wangjiuzhou (jiuzhou@shanshu.ai)
  * @date 2018/01/29
@@ -23,16 +21,19 @@ public class TestInteger extends TestCase {
         System.out.println(JSON.toJSONString(person));
     }
 
-    /**
-     * 测试整数的除法
-     */
-    public void test_percent(){
-        Integer i = 0;
-
-        Integer i1 = 0;
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-//        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        String format = decimalFormat.format((float) i / (float) i1);
-        System.out.println(format);
+    public void test2(){
+        Integer i = 2;
+        System.out.println(i.toString());
     }
+
+    public void test3(){
+        Integer i = 2147483647;
+//        System.out.println(i.compareTo(365) == 1);
+        if (i.compareTo(365) == 1){
+            System.out.println("大于365");
+        }else {
+            System.out.println("大于365");
+        }
+    }
+
 }
