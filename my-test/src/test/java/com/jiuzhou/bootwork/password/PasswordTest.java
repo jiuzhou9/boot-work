1d1e93886d4afd3b589a970c8ef95b88b324d45e
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-03-06 15:03:08 +0800

    增加postgresql/shiro 的module 但是尚未完成

---
+++ my-test/src/test/java/com/jiuzhou/bootwork/password/PasswordTest.java
@@ -0,0 +1,23 @@
+package com.jiuzhou.bootwork.password;
+
+import junit.framework.TestCase;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author wangjiuzhou (jiuzhou@shanshu.ai)
+ * @date 2018/03/05
+ */
+public class PasswordTest extends TestCase {
+
+    /**
+     * spring security 包中的加密
+     */
+    public void test() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String encode = bCryptPasswordEncoder.encode("12345");
+        System.out.println(encode);
+        boolean b = bCryptPasswordEncoder
+                        .matches("12345", encode);
+        System.out.println(b);
+    }
+}
