a7a1cf9a51d83dcad4d95fe38f75b3be8415baf6
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-03-23 10:46:34 +0800

    flyway维护脚本，auth-dao提供dao接口

--- rest-api/src/test/java/com/jiuzhou/bootwork/HelloApiTest.java
+++ rest-api/src/test/java/com/jiuzhou/bootwork/HelloApiTest.java
@@ -1,6 +1,5 @@
 package com.jiuzhou.bootwork;
 
-import com.alibaba.fastjson.JSON;
 import com.jiuzhou.bootwork.controller.HelloApi;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @date 2018/02/09
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ApiApp.class)
+@SpringBootTest(classes = RestApiApp.class)
 @WebAppConfiguration
 public class HelloApiTest {
 
