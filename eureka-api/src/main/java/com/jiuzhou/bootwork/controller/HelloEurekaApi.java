fdced98bf29ebe73162687d2a114d70b28f834be
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-04-12 12:00:38 +0800

    角色更新修复

--- eureka-api/src/main/java/com/jiuzhou/bootwork/controller/HelloEurekaApi.java
+++ eureka-api/src/main/java/com/jiuzhou/bootwork/controller/HelloEurekaApi.java
@@ -1,11 +1,8 @@
 package com.jiuzhou.bootwork.controller;
 
-import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
