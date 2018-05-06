440a44c2a6d79c3534f06d231b6a3046e2a77c38
jiuzhou9 <835540436@qq.com>
2018-05-06 19:43:00 +0800

    测试

--- auth-dao/src/main/java/com/jiuzhou/bootwork/App.java
+++ auth-dao/src/main/java/com/jiuzhou/bootwork/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Hello world!
  *
+ *
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.jiuzhou.bootwork.dao.mapper")
