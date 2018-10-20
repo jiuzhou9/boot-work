ece22003d8ac5e0b73c53aef29d1d28fb989abe5
Wang Jiuzhou <jiuzhou@shanshu.ai>
2018-10-20 23:13:11 +0800

    添加Redis模块

--- my-test/src/test/java/com/jiuzhou/bootwork/testabstract/Test.java
+++ my-test/src/test/java/com/jiuzhou/bootwork/testabstract/Test.java
@@ -1,7 +1,7 @@
 package com.jiuzhou.bootwork.testabstract;
 
 /**
- * @author wangjiuzhou (jiuzhou@shanshu.ai)
+ * @author wangjiuzhou (835540436@qq.com)
  * @date 2018/10/19
  */
 public class Test {
